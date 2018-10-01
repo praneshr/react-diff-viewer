c4ba49de7d27246219c11fe557ae5374161c74e9
Pranesh Ravi <praneshpranesh@gmail.com>
2018-10-01 11:34:19 +0530

    Adding build setup

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const css = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    main: './examples/src/index.tsx',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.jsx', '.tsx', '.ts', '.scss', '.css', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'examples/dist'),
+    filename: '[name].js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'examples/dist'),
+    port: 8000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.examples.json',
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          css.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './examples/src/index.ejs',
+    }),
+    new css({
+      filename: 'main.css',
+    }),
+  ]
+}
