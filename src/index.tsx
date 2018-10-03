492856d94f3c6293a22bee9b15d6f1b9c3921b35
Pranesh Ravi <praneshpranesh@gmail.com>
2018-10-03 16:00:59 +0530

    Adding support for word diff

--- src/index.tsx
+++ src/index.tsx
@@ -28,14 +28,28 @@ const beautifyValue = (source: string, customBeautify: (source: string) => strin
   }
 }
 
+const determineLineNumbers = (str: string) => str.split('\n').length
+
+const wordDiff = (str1: string, str2: string, hideType: string) => {
+  const charDiff = diff.diffWordsWithSpace(str1, str2)
+  return charDiff.map((obj: any) => {
+    if (obj[hideType]) return undefined
+    return <span className={cn(styles.wordDiff, { [styles.wordAdded]: obj.added, [styles.wordRemoved]: obj.removed })}>{obj.value}</span>
+  })
+}
 class DiffViewer extends React.PureComponent<DiffViewerProps, DiffViewerState> {
 
   splitView = (diffArray: any[]) => {
     let leftLineNumber = 0
     let rightLineNumber = 0
 
+
     return () => diffArray.map((obj: diff.IDiffResult, i) => {
-      return <div className={styles.diffContainer}>
+      const islineNumberNotEqual = obj.added
+        && diffArray[i - 1]
+        && diffArray[i - 1].removed
+        && (determineLineNumbers(diffArray[i - 1].value) !== determineLineNumbers(obj.value))
+      return <div className={cn(styles.diffContainer, { [styles.clearFix]: islineNumberNotEqual })}>
         <div className={styles.column}>
           {
             !obj.added
@@ -44,16 +58,20 @@ class DiffViewer extends React.PureComponent<DiffViewerProps, DiffViewerState> {
             .map((ch, num) => {
               rightLineNumber = rightLineNumber + 1
               let content: any = ch
-              // if (obj.removed && diffArray[i + 1] && diffArray[i + 1].added) {
-              //   const nextVal = diffArray[i + 1].value
-              //     .split('\n')
-              //     .filter(ch => ch.length > 0)[num]
-              //   content = nextVal ? this.wordDiff(ch, nextVal, 'added') : ch
-              // }
+              if (obj.removed && diffArray[i + 1] && diffArray[i + 1].added) {
+                const nextVal = diffArray[i + 1].value
+                  .split('\n')
+                  .filter((ch: string) => ch.length > 0)[num]
+                content = nextVal ? wordDiff(ch, nextVal, 'added') : ch
+              }
               return <div className={cn(styles.line, { [styles.diffRemoved]: obj.removed })}>
-                <span>
-                  {rightLineNumber}
-                  {obj.removed && <span>-</span>}
+                <span className={styles.gutter}>
+                  <span className={styles.lineNumber}>
+                    <pre>
+                      {rightLineNumber}
+                    </pre>
+                  </span>
+                  {obj.removed && <span className={styles.marker}>-</span>}
                 </span>
                 <pre>{content}</pre>
               </div>
@@ -68,16 +86,20 @@ class DiffViewer extends React.PureComponent<DiffViewerProps, DiffViewerState> {
             .map((ch, num) => {
               leftLineNumber = leftLineNumber + 1
               let content: any = ch
-              // if (obj.added && diffArray[i - 1] && diffArray[i - 1].removed) {
-              //   const preValue = diffArray[i - 1].value
-              //     .split('\n')
-              //     .filter(ch => ch.length > 0)[num]
-              //   content = preValue ? this.wordDiff(preValue, ch, 'removed') : ch
-              // }
+              if (obj.added && diffArray[i - 1] && diffArray[i - 1].removed) {
+                const preValue = diffArray[i - 1].value
+                  .split('\n')
+                  .filter((ch: string) => ch.length > 0)[num]
+                content = preValue ? wordDiff(preValue, ch, 'removed') : ch
+              }
               return <div className={cn(styles.line, { [styles.diffAdded]: obj.added })}>
-                <span>
-                  {leftLineNumber}
-                  {obj.added && <span>+</span>}
+                <span className={styles.gutter}>
+                  <span className={styles.lineNumber}>
+                    <pre>
+                      {leftLineNumber}
+                    </pre>
+                  </span>
+                  {obj.added && <span className={styles.marker}>+</span>}
                 </span>
                 <pre>{content}</pre>
               </div>
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,14 @@
 import { css } from 'emotion'
 
+const variables = {
+  addedBackground: '#E1FBDA',
+  addedColor: '#1B7C0E',
+  removedBackground: '#F5C9CF',
+  removedColor: '#A02A3B',
+  wordAddedBackground: '#97C391',
+  wordRemovedBackground: '#F48D8D',
+}
+
 export const diffContainer = css({
   'pre': {
     margin: 0,
@@ -14,15 +23,59 @@ export const column = css({
 })
 
 export const diffRemoved = css({
-  background: 'red',
-  // color: 'red',
+  background: variables.removedBackground,
+  color: variables.removedColor,
+  pre: {
+    color: variables.removedColor,
+  },
 })
 
 export const diffAdded = css({
-  background: 'green',
-  // color: 'green',
+  background: variables.addedBackground,
+  color: variables.addedColor,
+  pre: {
+    color: variables.addedColor,
+  },
+})
+
+export const marker = css({
+  marginTop: -4,
+  userSelect: 'none',
+})
+
+export const gutter = css({
+  minWidth: 40,
+  display: 'flex',
+  marginRight: 20,
+  justifyContent: 'space-between',
+})
+
+export const lineNumber = css({
+  opacity: 0.3,
+  userSelect: 'none',
+})
+
+export const clearFix = css({
+  '&:after': {
+    content: '" "',
+    display: 'table',
+    clear: 'both',
+  }
 })
 
 export const line = css({
   display: 'flex',
+  padding: '2px 0',
+})
+
+export const wordDiff = css({
+  padding: 2,
+})
+
+export const wordAdded = css({
+  background: variables.wordAddedBackground,
+})
+
+export const wordRemoved = css({
+  background: variables.wordRemovedBackground,
 })
