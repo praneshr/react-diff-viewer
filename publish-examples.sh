64207ee739c7abec8a1186a1b8cacebc90a56656
Pranesh Ravi <praneshpranesh@gmail.com>
2019-11-24 06:56:54 +0530

    Update publish-examples.sh

--- publish-examples.sh
+++ publish-examples.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "Publishing examples to Github pages...."
-
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
-yarn publish:examples
+if [ -n "$TRAVIS_TAG" ]; then
+  echo "Publishing examples to Github pages...."
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  yarn publish:examples
+else
+  echo "Skipping examples deployment..."
+fi
